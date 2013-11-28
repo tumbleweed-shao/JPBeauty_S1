e49206afd49607672ffbc2e1c3a9c9fd95383294
tumbleweed-shao <Paul@Paul-PC>
2013-11-28 15:41:21 +0800

    Remove the AD part, this version is the original v1.0

--- src/com/jpbeauty/jpbeauty_s1/JPBeautyMainActivity.java
+++ src/com/jpbeauty/jpbeauty_s1/JPBeautyMainActivity.java
@@ -31,8 +31,6 @@ public class JPBeautyMainActivity extends Activity {
 	ImageView largeIV;
 	WallpaperManager wallpaperManager;
 	Integer[] mImageIds;
-	DomobAdView mAdview320x50;// AD
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -58,7 +56,6 @@ public class JPBeautyMainActivity extends Activity {
 		largeIV.setTag(initPicPath);
 		wallpaperManager = WallpaperManager.getInstance(this);
 		initGalarry();
-		InitAD();
 	}
 
 	private void InitController() {
@@ -92,59 +89,6 @@ public class JPBeautyMainActivity extends Activity {
 		WindowManager wm = (WindowManager) context
 				.getSystemService(Context.WINDOW_SERVICE);
 		return wm.getDefaultDisplay();
-	}
-
-	private void InitAD() {
-		LinearLayout mAdContainer = (LinearLayout) findViewById(R.id.lineAD);
-		mAdview320x50 = new DomobAdView(JPBeautyMainActivity.this,
-				"56OJw3touNPeAuwIFK", "16TLu49vApI7YNU-pQtVn5Bk",
-				DomobAdView.INLINE_SIZE_320X50);
-		mAdview320x50.setKeyword("game");
-		mAdview320x50.setUserGender("male");
-		mAdview320x50.setUserBirthdayStr("2000-08-08");
-		mAdview320x50.setUserPostcode("123456");
-		mAdview320x50.setAdEventListener(new DomobAdEventListener() {
-
-			@Override
-			public void onDomobAdReturned(DomobAdView adView) {
-				// Log.i("DomobSDKDemo", "onDomobAdReturned");
-			}
-
-			@Override
-			public void onDomobAdOverlayPresented(DomobAdView adView) {
-				// Log.i("DomobSDKDemo", "overlayPresented");
-			}
-
-			@Override
-			public void onDomobAdOverlayDismissed(DomobAdView adView) {
-				// Log.i("DomobSDKDemo", "Overrided be dismissed");
-			}
-
-			@Override
-			public void onDomobAdClicked(DomobAdView arg0) {
-				// Log.i("DomobSDKDemo", "onDomobAdClicked");
-			}
-
-			@Override
-			public void onDomobAdFailed(DomobAdView arg0, ErrorCode arg1) {
-				// Log.i("DomobSDKDemo", "onDomobAdFailed");
-			}
-
-			@Override
-			public void onDomobLeaveApplication(DomobAdView arg0) {
-				// Log.i("DomobSDKDemo", "onDomobLeaveApplication");
-			}
-
-			@Override
-			public Context onDomobAdRequiresCurrentContext() {
-				return JPBeautyMainActivity.this;
-			}
-		});
-		RelativeLayout.LayoutParams layout = new RelativeLayout.LayoutParams(
-				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		layout.addRule(RelativeLayout.CENTER_HORIZONTAL);
-		mAdview320x50.setLayoutParams(layout);
-		mAdContainer.addView(mAdview320x50);
 	}
 
 	private void copyBeautyToSD() {
